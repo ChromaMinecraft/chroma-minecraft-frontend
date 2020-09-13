7c7ed170898171042ed1944fa1f56ee605215059
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2020-09-14 02:47:40 +0700

    fixed center cover

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,11 +11,10 @@ export default function Home() {
         ></link>
       </Head>
       <div
-        className="z-0 min-h-screen bg-cover bg-center static"
+        className="z-0 min-h-screen bg-cover bg-center static "
         style={{
           backgroundImage:
-            'url(https://files.wallpaperpass.com/2019/10/minecraft%20wallpaper%2006%20-%201920x1080.jpg)',
-          WebkitFilter: 'blur(5px)',
+            'url(https://images5.alphacoders.com/108/thumb-1920-1081389.png)',
         }}
       ></div>
       <div
@@ -27,16 +26,31 @@ export default function Home() {
           style={{
             background: 'rgba(0,0,0,0.4)',
           }}
-        >
-          <h2 className="font-semibold text-5xl">Chroma Minecraft</h2>
-          <h4 className="font-medium text-3xl">
-            Server minecraft survival yang mendukung berbagai platform
-            (Java/Bedrock/Pocket Edition).
-          </h4>
-          <h4 className="font-light text-2xl">
-            Temukan keseruan sensasi berpetualang di dunia minecraft yang penuh
-            dengan misteri, harta karun, village, dan lainnya.
-          </h4>
+        ></div>
+      </div>
+      <div
+        className="z-20 absolute text-center text-white top-50 left-50 w-screen align-middle sm:px-16"
+        style={{ transform: 'translate(-50%, -50%)' }}
+      >
+        <h4 className="font-semibold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          Chroma Minecraft
+        </h4>
+        <h4 className="font-medium xs:text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+          Server minecraft survival yang mendukung berbagai platform
+          (Java/Bedrock/Pocket Edition).
+        </h4>
+        <h4 className="font-light xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          Temukan keseruan sensasi berpetualang di dunia minecraft yang penuh
+          dengan misteri, harta karun, village, dan lainnya.
+        </h4>
+        <div className="my-16"></div>
+        <div className="flex flex-column justify-center sm:px-16">
+          <a
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            href="tes"
+          >
+            tes
+          </a>
         </div>
       </div>
     </>
