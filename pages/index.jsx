c8daeb3322eefb8ba54fb06c511d73da718e5033
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2020-09-15 03:56:32 +0700

    icon done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,13 @@
 import Head from 'next/head';
+<script src="https://kit.fontawesome.com/a6da6b3a40.js" crossorigin="anonymous"></script>
 
 export default function Home() {
-  const icons = [{ value: 'Discord' }];
+  const icons = [
+    { text: 'Discord', id: 'discord', src: 'icon/discord.svg', url: 'http://discord.chroma-gaming.xyz', target: '_blank'},
+    { text: 'Play', id: 'play', src: 'icon/play-button.svg', url: '#', target: '_self' },
+    { text: 'Vote', id: 'vote', src: 'icon/vote.svg', url: 'https://minecraft-mp.com/server/268502/vote/', target: '_blank' },
+
+  ];
 
   return (
     <>
@@ -16,7 +22,7 @@ export default function Home() {
         className="z-0 min-h-screen bg-cover bg-center static "
         style={{
           backgroundImage:
-            'url(https://images5.alphacoders.com/108/thumb-1920-1081389.png)',
+            'url("img/bg.png")',
         }}
       ></div>
       <div
@@ -46,14 +52,24 @@ export default function Home() {
           dengan misteri, harta karun, village, dan lainnya.
         </h4>
         <div className="my-16"></div>
-        <div className="flex flex-column justify-center sm:px-16">
-          {icons.map(({ value }) => (
-            <a
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              href="tes"
-            >
-              {value}
-            </a>
+        <div className="flex flex-row justify-center sm:px-16">
+          {icons.map(({ text, id, src, url, target }) => (
+            <div className="mx-10 xs:mx-6 flex flex-col justify-center">
+              <div className="flex flex-row justify-center">
+                <a
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-5 rounded-full h-20 w-20"
+                  href={url}
+                  target={target}
+                >
+                  <div className="w-full">
+                    <img src={src} alt="" />
+                  </div>
+                </a>
+              </div>
+              <div>
+                <h1 className="xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">{text}</h1>
+              </div>
+            </div>
           ))}
         </div>
       </div>
