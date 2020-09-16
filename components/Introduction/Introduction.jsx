043fec1c9b9e2d23579400331ae6b2941e0fb270
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2020-09-16 21:27:47 +0700

    add tooltip

--- components/Introduction/Introduction.jsx
+++ components/Introduction/Introduction.jsx
@@ -1,42 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { contents, icons } from './index.config';
-import Axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { contents, icons } from "./index.config";
+import Axios from "axios";
+import ReactTooltip from "react-tooltip";
 
 const Introduction = () => {
   const [playerCount, setPlayerCount] = useState(0);
-  
+  const [playerList, setPlayerList] = useState(0);
+
   useEffect(() => {
     const getPlayersCount = async () => {
-      const { data } = await Axios.get('https://api.mcsrvstat.us/2/mc.chroma-gaming.xyz');
+      const { data } = await Axios.get(
+        "https://api.mcsrvstat.us/2/mc.chroma-gaming.xyz"
+      );
+      let playerArr = [];
+      data.players.list.map(e => {
+        playerArr.push(e);
+      });
       setPlayerCount(data.players.online);
-    }
-    getPlayersCount();    
-  }, [])
+      setPlayerList(playerArr.join("<br>"));
+      ReactTooltip.rebuild();
+    };
+    getPlayersCount();
+  }, []);
 
   return (
     <>
       <div
         className="z-0 min-h-screen bg-cover bg-center static "
         style={{
-          backgroundImage: 'url("img/bg.png")',
+          backgroundImage: 'url("img/bg.png")'
         }}
       ></div>
       <div
         className="z-10 absolute text-center text-white top-50 left-50 w-screen align-middle"
         style={{
-          transform: 'translate(-50%, -50%)',
+          transform: "translate(-50%, -50%)"
         }}
       >
         <div
           className="min-h-screen"
           style={{
-            background: 'rgba(0, 0, 0, 0.4)',
+            background: "rgba(0, 0, 0, 0.4)"
           }}
         ></div>
       </div>
       <div
         className="z-20 absolute text-center text-white top-50 left-50 w-screen align-middle sm:px-16"
-        style={{ transform: 'translate(-50%, -50%)' }}
+        style={{ transform: "translate(-50%, -50%)" }}
       >
         <h4 className="font-semibold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
           {contents.title}
@@ -48,7 +58,20 @@ const Introduction = () => {
           {contents.subTitle[1]}
         </h4>
         <h4 className="font-light xs:text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-5">
-          <span className="font-bold">{playerCount} Pemain</span> sedang bermain saat ini
+          <a className="font-bold" data-tip={playerList} data-class="font-bold" data-for="playerTooltip" data-multiline="true">
+            {playerCount} Pemain
+          </a>{" "}
+          sedang bermain saat ini <br></br>
+          <span className="xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
+            (Arahkan mouse ke jumlah pemain untuk melihat daftar pemain)
+          </span>
+          <ReactTooltip
+            id="playerTooltip"
+            type="light"
+            effect="solid"
+            place="bottom"
+          >
+          </ReactTooltip>
         </h4>
         <div className="flex flex-row justify-center sm:px-16 my-12">
           {icons.map(
@@ -63,7 +86,7 @@ const Introduction = () => {
                     id={id}
                     href={url}
                     target={target}
-                    onClick={(e) => onClick(e, url)}
+                    onClick={e => onClick(e, url)}
                   >
                     {content}
                   </a>
