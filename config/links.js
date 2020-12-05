f6b121d767a4c8d0d4c576a1f374a8b9b5e76a11
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2020-12-05 20:09:11 +0700

    add page donate

---
+++ config/links.js
@@ -0,0 +1,6 @@
+export const LIST_URL = {
+  DONATE: 'https://discord.gg/hDrwCsfWqq',
+  STATUS: 'https://api.mcsrvstat.us/2/mc.chroma-gaming.xyz',
+  SERVER: 'http://discord.chroma-gaming.xyz',
+  VOTE: 'https://minecraft-mp.com/server/268676/vote/',
+};
---
+++ pages/donate/index.jsx
@@ -0,0 +1,4 @@
+import redirect from 'nextjs-redirect';
+import { LIST_URL } from '../../config/links';
+
+export default redirect(LIST_URL.DONATE);
