3e0c02a10ef61a812718b728554b6d8e7ca326aa
Aldiwildan <aldiwild77@gmail.com>
2021-04-23 03:22:15 +0700

    Update constant.js

--- utils/constant.js
+++ utils/constant.js
@@ -3,7 +3,7 @@ import { FaDiscord, FaPlay, FaPoll, FaAward, FaDonate } from "react-icons/fa";
 export const LIST_URL = {
   DONATE: "https://discord.gg/hDrwCsfWqq",
   STATUS: "https://api.mcsrvstat.us/2/mc.chroma-gaming.xyz",
-  SERVER: "http://discord.chroma-gaming.xyz",
+  SERVER: "https://discord.chroma-gaming.xyz",
   VOTE: "https://minecraft-mp.com/server/268676/vote/",
   LEADERBOARD:
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTWSqDNAbqLY2t3pVX-nFeA3604PtzZMvelCeRdZqx4cVnVNQlXaFL66yJ3ajld4oTAd6CUHX_UKqGh/pubchart?oid=25596175&amp;format=interactive",
