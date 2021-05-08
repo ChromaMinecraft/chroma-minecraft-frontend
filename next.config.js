625082a00dcf0e22b727e650c5390acdcc5d95e7
Fakhri1999 <fakhriimaduddin19@gmail.com>
2021-05-09 02:33:12 +0700

    add google analytics

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const compose = require("compose-function");
-const { withDokz } = require("dokz/dist/plugin");
+const compose = require('compose-function');
+const { withDokz } = require('dokz/dist/plugin');
 
 const composed = compose(withDokz);
 
 module.exports = composed({
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  env: {
+    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
+  },
 });
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,70 +1,83 @@
-import "../styles/globals.css";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { DokzProvider, ColorModeSwitch, GithubLink, Link } from "dokz";
-import { ChakraProvider } from "@chakra-ui/react";
-import DiscordLink from "../components/DiscordLink";
-import { GoogleFonts } from "next-google-fonts";
-import theme from "../chakra.config";
+import '../styles/globals.css';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { DokzProvider, ColorModeSwitch, GithubLink, Link } from 'dokz';
+import { ChakraProvider } from '@chakra-ui/react';
+import { GoogleFonts } from 'next-google-fonts';
+import { useEffect } from 'react';
+
+import DiscordLink from '../components/DiscordLink';
+
+import theme from '../chakra.config';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
-  const { pathname } = useRouter();
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
-  if (pathname.startsWith("/wiki")) {
+  if (router.pathname.startsWith('/wiki')) {
     return (
       <ChakraProvider>
         <Head>
           <title>Chroma Minecraft</title>
-          <meta charSet="UTF-8" />
+          <meta charSet='UTF-8' />
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
           />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta httpEquiv='x-ua-compatible' content='ie=edge' />
           <meta
-            property="og:title"
-            content="Chroma Minecraft"
-            key="meta-title"
+            property='og:title'
+            content='Chroma Minecraft'
+            key='meta-title'
           />
           <meta
-            property="og:description"
-            content="Server minecraft survival yang mendukung berbagai platform (Java / Bedrock / Pocket Edition)."
-            key="meta-description"
+            property='og:description'
+            content='Server minecraft survival yang mendukung berbagai platform (Java / Bedrock / Pocket Edition).'
+            key='meta-description'
           />
-          <meta property="og:image" content="/img/logo.png" />
-          <meta property="og:locale" content="id_ID" key="meta-locale" />
+          <meta property='og:image' content='/img/logo.png' />
+          <meta property='og:locale' content='id_ID' key='meta-locale' />
           <meta
-            property="og:locale:alternate"
-            content="en_US"
-            key="meta-locale-alt"
+            property='og:locale:alternate'
+            content='en_US'
+            key='meta-locale-alt'
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Fira+Code"
-            rel="stylesheet"
-            key="google-font-Fira"
+            href='https://fonts.googleapis.com/css?family=Fira+Code'
+            rel='stylesheet'
+            key='google-font-Fira'
           />
         </Head>
         <DokzProvider
           animate
-          docsRootPath="pages/wiki"
-          headTitlePrefix="Chroma Minecraft Wiki - "
+          docsRootPath='pages/wiki'
+          headTitlePrefix='Chroma Minecraft Wiki - '
           headerLogo={
             <img
-              src="/img/logo.png"
+              src='/img/logo.png'
               style={{ opacity: 1 }}
-              width="50px"
-              height="50px"
+              width='50px'
+              height='50px'
             />
           }
           headerItems={[
-            <DiscordLink url="https://discord.chroma-gaming.xyz" />,
-            <GithubLink url="https://github.com/ChromaMinecraft" />,
-            <ColorModeSwitch key="1" />,
+            <DiscordLink url='https://discord.chroma-gaming.xyz' />,
+            <GithubLink url='https://github.com/ChromaMinecraft' />,
+            <ColorModeSwitch key='1' />,
           ]}
           sidebarOrdering={{
-            "index.mdx": true,
+            'index.mdx': true,
             wiki: {
-              "index.mdx": true,
+              'index.mdx': true,
               rules: true,
               tutorial: true,
             },
@@ -81,26 +94,26 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider theme={theme}>
       <Head>
         <title>Chroma Minecraft</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta property="og:title" content="Chroma Minecraft" key="meta-title" />
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta property='og:title' content='Chroma Minecraft' key='meta-title' />
         <meta
-          property="og:description"
-          content="Server minecraft survival yang mendukung berbagai platform (Java / Bedrock / Pocket Edition)."
-          key="meta-description"
+          property='og:description'
+          content='Server minecraft survival yang mendukung berbagai platform (Java / Bedrock / Pocket Edition).'
+          key='meta-description'
         />
-        <meta property="og:image" content="/img/logo.png" />
-        <meta property="og:locale" content="id_ID" key="meta-locale" />
+        <meta property='og:image' content='/img/logo.png' />
+        <meta property='og:locale' content='id_ID' key='meta-locale' />
         <meta
-          property="og:locale:alternate"
-          content="en_US"
-          key="meta-locale-alt"
+          property='og:locale:alternate'
+          content='en_US'
+          key='meta-locale-alt'
         />
       </Head>
       <GoogleFonts
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
-        rel="stylesheet"
+        href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap'
+        rel='stylesheet'
       />
       <Component {...pageProps} />
     </ChakraProvider>
